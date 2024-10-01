7e84f4ac2aa24429b6f0bed27bea36cec3c3ef4f
Omar Abdelhadi <omarabdelhadi@cmail.carleton.ca>
2024-10-01 13:11:39 -0400

    changed base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/", // leave empty for custom domain name
+  base: "/Personal-Website/", // leave empty for custom domain name
 });
