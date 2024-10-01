3bb2d5344ce8ddd9215b1492df1822162b580ed0
Omar Abdelhadi <omarabdelhadi@cmail.carleton.ca>
2024-10-01 15:00:28 -0400

    custom domain reattempt

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Personal-Website/", // leave empty for custom domain name
+  base: "/omarabdelhadi.com/", // leave empty for custom domain name
 });
