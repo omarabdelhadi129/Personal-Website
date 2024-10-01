e147fc98db67a97b4b733f9f31b8267e59176730
Omar Abdelhadi <omarabdelhadi@cmail.carleton.ca>
2024-10-01 09:51:22 -0400

    configuring for cname

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Personal-Website/", // Add your repository name here
+  base: "/", // leave empty for custom domain name
 });
